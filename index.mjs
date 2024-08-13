1f8f999496ede9dffce9ef018daafd5c9ba7e8dd
papd56 <papd56@plmyh.com>
2024-08-13 13:30:11 +0800

    群管理机器人

--- index.mjs
+++ index.mjs
@@ -1,149 +1,156 @@
 import TelegramBot from 'node-telegram-bot-api';
-const token = "7269675720:AAEEkkXm30WMsjR4ZWysHDPQTQeym0aUX-Y";
+
+const token = '7269675720:AAEEkkXm30WMsjR4ZWysHDPQTQeym0aUX-Y';
+import checkifUserIsAdmin from './adminCheck.mjs';
+
 const bot = new TelegramBot(token, {
-    polling: true,
+  polling: true,
 });
 
-bot.on("message", async (msg) => {
-    if (!msg || !msg.chat) {
-        console.log('无效的消息结构:', msg);
-        return false;
-    }
-    console.log('有效的消息结构:', msg);
-    const chat_id = msg.chat.id;
-    const user_id = msg.from.id;
-    const messageText = msg.text;
-    const orignalMessage = msg.message_id; //获取原始消息ID
-    let replyMessage = msg.message_id; //获取回复消息ID
-    if (msg.reply_to_message) {
-        replyMessage = msg.reply_to_message.message_id; //获取回复消息ID
-    }
-    // 设置权限 (允许发送消息和图片)
-    const newPermissions = {
-        can_send_messages: true,
-        can_send_photos: true,
-        // ...其他权限设置
-    };
+bot.on('message', async (msg) => {
+  if (!msg || !msg.chat) {
+    console.log('无效的消息结构:', msg);
+    return false;
+  }
+  console.log('有效的消息结构:', msg);
+  const chatId = msg.chat.id;
+  const userId = msg.from.id;
+  const messageText = msg.text;
+  const messageId = msg.message_id; //获取消息ID
+  const replyMessage = msg.reply_to_message;
+  let replyMessageId = messageId;
+  let replyUserId = userId;
+  if (replyMessage) {
+    replyMessageId = replyMessage.message_id; //获取回复消息ID
+    replyUserId = replyMessage.from.id; //获取回复用户ID
+  }
+  // 设置权限 (允许发送消息和图片)
+  const newPermissions = {
+    can_send_messages: true,
+    can_send_photos: true,
+    // ...其他权限设置
+  };
 
-    try {
-        if (messageText === "验群") {
-            await bot.sendMessage(chat_id, "本群是真群！请注意看我的用户名是 @Guik88 (群管拼音)，谨防假机器人。私聊我输入词语可以搜索真公群,如：卡商、白资、承兑等。请找有头衔的人在群内交易，切勿相信主动私聊你的，都是骗子。非群内交易没有任何保障.", {
-                reply_to_message_id: orignalMessage,
-            });
+  try {
+    // 检查消息是否来自群组
+    if (msg.chat.type === 'group' || msg.chat.type === 'supergroup') {
+      if (messageText === '验群') {
+        await bot.sendMessage(chatId, '本群是真群！请注意看我的用户名是 @Guik88 (群管拼音)，谨防假机器人。私聊我输入词语可以搜索真公群,如：卡商、白资、承兑等。请找有头衔的人在群内交易，切勿相信主动私聊你的，都是骗子。非群内交易没有任何保障.', {
+          reply_to_message_id: messageId,
+        });
+      }
+      let isAdmin = await checkifUserIsAdmin(bot, msg);
+      if (messageText && isAdmin === 1) {
+        if (messageText.startsWith('修改公群名')) {
+          // 更改群组名称
+          await bot.setChatTitle(chatId, messageText.substring(5));
+          await bot.sendMessage(chatId, '修改成功', { reply_to_message_id: messageId });
         }
 
-    } catch (error) {
-        console.log("获取信息出错", error);
-    }
-
-    try {
-        if (messageText === "上课") {
-            const isAdmin = await checkIsNoUserIsAdmin(msg);
-            if (isAdmin === 1) {
-                //群已开  发送消息 发送媒体
-                bot.setChatPermissions(chat_id, newPermissions);
-                bot.sendMessage(chat_id, "群已开，群内可以正常营业", {
-                    reply_to_message_id: orignalMessage,
-
-
-                });
-            }
+        if (messageText === '真公群') {
+          //真公群  发送消息 发送图片
+          await bot.setChatPermissions(chatId, newPermissions);
+          await bot.sendMessage(chatId, '该公群为真公群！', {
+            reply_to_message_id: messageId,
+          });
         }
 
-        if (messageText === "置顶" && msg.reply_to_message) {
-            const isAdmin = await checkIsNoUserIsAdmin(msg);
-            if (isAdmin === 1) {
-                // 置顶命令事件监听
-                console.log("置顶监听事件" + isAdmin)
-                // 发送消息并提供自定义键盘
-                await bot.pinChatMessage(chat_id, replyMessage, {});
+        if (messageText === '上课') {
+          //群已开  发送消息 发送图片
+          await bot.setChatPermissions(chatId, newPermissions);
+          await bot.sendMessage(chatId, '群已开，群内可以正常营业', {
+            reply_to_message_id: messageId,
+          });
+        }
 
-                // 发送置顶提醒
-                await bot.sendMessage(chat_id, "置顶成功", {
-                    reply_to_message_id: orignalMessage
-                });
-                /*                 bot.onText("置顶", async () => {
-                                    // const messageId = match[1];
-                                    console.log("消息 messageid");
-                                    // 创建自定义键盘
-                                    const options = {
-                                        reply_markup: JSON.stringify({
-                                            inline_keyboard: [
-                                                [{ text: "置顶", callback_data: "pin" }]
-                                            ]
-                                        })
-                                    };
-                                    // 发送消息并提供自定义键盘
-                                    await bot.pinChatMessage(chat_id, orignalMessage, options);
-                
-                                    // 发送置顶提醒
-                                    await bot.sendMessage(chat_id, "置顶成功", {
-                                        reply_to_message_id: orignalMessage
-                                    });
-                                }); */
+        if (messageText === '下课') {
+          //设置全员禁言
+          await bot.setChatPermissions(chatId, { can_send_messages: false });
+          await bot.sendMessage(chatId, '本公群今日已下课，如需交易，请在该群恢复营业后在群内交易！ 切勿私下交易！！！如有业务咨询请联系群老板/业务员如有纠纷请联系纠纷专员 @Guik88', {
+            reply_to_message_id: messageId,
+          });
+        }
 
-                // 处理用户输入"置顶"
-                // bot.onText(/置顶/, async (msg, match) => {
-                //     const messageIds = match[1];
-                //     // 创建自定义键盘
-                //     const options = {
-                //         reply_markup: JSON.stringify({
-                //             inline_keyboard: [
-                //                 [{ text: "置顶", callback_data: "pin" }]
-                //             ]
-                //         })
-                //     };
-                //     console.log(messageIds);
-                //     // 发送消息并提供自定义键盘
-                //     bot.pinChatMessage(chat_id, messageIds, options);
-                // });
+        if (messageText.startsWith('设置群老板 @') || messageText.startsWith('设置群业务员 @')) {
+          /*         const username = msg.text.split('@')[1];
+                  // 获取群组所有成员
+                  const chatMembers = await bot.getChatMembers(chatId);
+                  // 在群组成员中查找匹配用户名的用户
+                  const targetUser = chatMembers.find(member => member.user.username === username);
+                  const user = await bot.getChatMember(chatId, `@${username}`); */
+          //设置群老板管理员权限
+          await bot.promoteChatMember(chatId, userId, { can_delete_messages: true });
+          if (messageText.startsWith('设置群老板 @')) {
+            await bot.setChatAdministratorCustomTitle(chatId, replyUserId, '本公群老板，小心骗子假冒！');
+          } else {
+            await bot.setChatAdministratorCustomTitle(chatId, replyUserId, '本公群业务员，小心骗子假冒！');
+          }
+          // 发送置顶提醒
+          if (messageText.startsWith('设置群老板 @')) {
+            await bot.sendMessage(chatId, '设置群老板成功', { reply_to_message_id: messageId });
+          } else {
+            await bot.sendMessage(chatId, '设置群业务员成功', { reply_to_message_id: messageId });
+          }
+        }
 
-                // // 处理回调查询
-                // bot.on('callback_query', async (query) => {
-                //     if (query.data === 'pin') {
-                //         // 用户点击了“置顶”按钮
-                //         await bot.answerCallbackQuery(query.id, { text: "请手动置顶消息。" });
-                //     }
-                // });
-
+        if (replyMessage) {
+          if (messageText === '设置群老板' || messageText === '设置群业务员') {
+            //设置群老板管理员权限
+            await bot.promoteChatMember(chatId, replyUserId, { can_delete_messages: true });
+            if (messageText === '设置群老板') {
+              await bot.setChatAdministratorCustomTitle(chatId, replyUserId, '本公群老板，小心骗子假冒！');
             } else {
-                bot.sendMessage(chat_id, "只有管理员才能使用此命令。");
+              await bot.setChatAdministratorCustomTitle(chatId, replyUserId, '本公群业务员，小心骗子假冒！');
             }
-        }
+            // 发送置顶提醒
+            if (messageText === '设置群老板') {
+              await bot.sendMessage(chatId, '设置群老板成功', { reply_to_message_id: messageId });
+            } else {
+              await bot.sendMessage(chatId, '设置群业务员成功', { reply_to_message_id: messageId });
+            }
+          }
 
-        if (messageText === "下课") {
-            const isAdmin = await checkIsNoUserIsAdmin(msg);
-            if (isAdmin === 1) {
-                //设置全员禁言
-                bot.setChatPermissions(chat_id, { can_send_messages: false });
-                bot.sendMessage(chat_id, "本公群今日已下课，如需交易，请在该群恢复营业后在群内交易！ 切勿私下交易！！！如有业务咨询请联系群老板/业务员如有纠纷请联系纠纷专员 @Guik88", {
-                    reply_to_message_id: orignalMessage,
-                });
-            }
+          if (messageText === '开启权限') {
+            await bot.promoteChatMember(chatId, userId, {
+              can_change_info: true,        // 修改群组信息
+              can_delete_messages: true,    // 删除信息
+              can_restrict_members: true,   // 封禁成员
+              can_invite_users: true,       // 添加成员
+              can_pin_messages: true,       // 置顶消息
+              can_promote_members: true     // 添加管理员
+            });
+            await bot.sendMessage(chatId, '添加成功', {
+              reply_to_message_id: messageId,
+            });
+          }
+
+          if (messageText === '置顶') {
+            await bot.pinChatMessage(chatId, replyMessageId, {});
+            // 发送置顶提醒
+            await bot.sendMessage(chatId, '置顶成功', {
+              reply_to_message_id: messageId
+            });
+          }
+
+          if (messageText === '取消置顶') {
+            await bot.unpinChatMessage(chatId, {
+              message_id: replyMessageId
+            });
+            // 发送取消置顶提醒
+            await bot.sendMessage(chatId, '取消置顶成功', {
+              reply_to_message_id: messageId
+            });
+          }
         }
-    } catch (error) {
-        console.log("获取信息出错");
-        throw error
+      } else if (isAdmin === 0) {
+        await bot.sendMessage(chatId, '只有管理员才能使用此命令。');
+      }
+    } else {
+      await bot.sendMessage(chatId, '这个命令只能在群组中使用。');
     }
-
+  } catch (error) {
+    console.log('获取信息出错');
+    throw error;
+  }
 });
 
-
-async function checkIsNoUserIsAdmin(msg) {
-    const chat_id = msg.chat.id;
-    const user_id = msg.from.id;
-    try {
-        const ChatMember = await bot.getChatMember(chat_id, user_id);
-        if (
-            ChatMember.status === "administrator" ||
-            ChatMember.status === "creator"
-        ) {
-            return 1;
-        } else {
-            return 0;
-        }
-    } catch (error) {
-        console.error("获取成员信息出错：", error)
-        throw error;
-    }
-}

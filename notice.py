36a75adc27f777ebfc21d37ae3d551843e88ec99
hawkins <bashen976@gmail.com>
2024-11-04 22:22:46 +0800

    11

--- notice.py
+++ notice.py
@@ -7,19 +7,26 @@ BOT_TOKEN = '7829636771:AAGpnFhb8wpA1oEPHEzXBudxMoAMUZOP2ng'
 bot = telebot.TeleBot(BOT_TOKEN)
 
 # 要发送的消息
-message = "金秋十月，神奇科比华丽上线了，欢迎大家咨询：@GPP331201, @xiaoshi889766"
+message = "神奇猫猫全球同步钓鱼网站出租 包裹，网银，积分，电商支付插件。承接私人定制玩法保密你知我知支持各大担保上押，支持先做后付 👍 价格天66u 周450u月 永久1800u包含服务器域名搭建。 合作联系：@xiaoshi889766"
 
 # 用户 ID 或群组 ID 列表
-user_ids = [-1002199875824, -1002456247839,7445077710 ,6884995168]
+user_ids = [-1002199875824, -1002456247839,7445077710 ,6884995168,-1002160373359]
 
 # 发送消息
-for user_id in user_ids:
+def send_message(user_id, message):
     try:
         bot.send_message(user_id, message)
-        print(f"消息发送给 {user_id} 成功")
-        time.sleep(1)  # 每次发送后延迟1秒
     except Exception as e:
-        print(f"发送消息失败: {e}")
+        print(f"发送消息出错: {e}")
+
+while True:
+    try:
+        for user_id in user_ids:
+            send_message(user_id, message)
+            print(f"消息发送给 {user_id}")
+    except Exception as e:
+        print(f"发送消息出错: {e}")
+    time.sleep(10)
 
 # 开启轮询（一般不需要）
 bot.polling()
